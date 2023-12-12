195070bda3baa6b7451e83c26eec9fe2037da68b
yashmali408 <yashmali408@gmail.com>
2023-12-12 14:49:43 +0530

    yash

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+students = {
+    "s1" :{ "name":"yash","surname":"mali","address":"neemch" },
+    "s2" :{ "name":"gourav","surname":"beragi","address":"ratlam" },
+    "s3" :{ "name":"manas","surname":"tiwari","address":"ujjain" },
+    "s4" :{ "name":"sonu","surname":"prajapati","address":"jaora" }
+}
+
+
+for student in students:
+    print(f"hello{students[student]["name"]} {students[student]["surname"]} {students[student]["address"]}") 
